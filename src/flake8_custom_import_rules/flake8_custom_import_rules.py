024165973f87dd5d324d8a9143108b9a19d4bc1f
Rodrigo Gonzalez <r@rodrigo-gonzalez.com>
2023-04-24 21:02:43 -0400

    build: adding pre-commit hooks and tests (#2)

--- src/flake8_custom_import_rules/flake8_custom_import_rules.py
+++ src/flake8_custom_import_rules/flake8_custom_import_rules.py
@@ -5,16 +5,18 @@ import sys
 from typing import Any
 from typing import Generator
 
-YTT101 = 'YTT101 `sys.version[:3]` referenced (python3.10), use `sys.version_info`'  # noqa: E501
-YTT102 = 'YTT102 `sys.version[2]` referenced (python3.10), use `sys.version_info`'  # noqa: E501
-YTT103 = 'YTT103 `sys.version` compared to string (python3.10), use `sys.version_info`'  # noqa: E501
-YTT201 = 'YTT201 `sys.version_info[0] == 3` referenced (python4), use `>=`'
-YTT202 = 'YTT202 `six.PY3` referenced (python4), use `not six.PY2`'
-YTT203 = 'YTT203 `sys.version_info[1]` compared to integer (python4), compare `sys.version_info` to tuple'  # noqa: E501
-YTT204 = 'YTT204 `sys.version_info.minor` compared to integer (python4), compare `sys.version_info` to tuple'  # noqa: E501
-YTT301 = 'YTT301 `sys.version[0]` referenced (python10), use `sys.version_info`'  # noqa: E501
-YTT302 = 'YTT302 `sys.version` compared to string (python10), use `sys.version_info`'  # noqa: E501
-YTT303 = 'YTT303 `sys.version[:1]` referenced (python10), use `sys.version_info`'  # noqa: E501
+YTT101 = "YTT101 `sys.version[:3]` referenced (python3.10), use `sys.version_info`"  # noqa: E501
+YTT102 = "YTT102 `sys.version[2]` referenced (python3.10), use `sys.version_info`"  # noqa: E501
+YTT103 = (
+    "YTT103 `sys.version` compared to string (python3.10), use `sys.version_info`"  # noqa: E501
+)
+YTT201 = "YTT201 `sys.version_info[0] == 3` referenced (python4), use `>=`"
+YTT202 = "YTT202 `six.PY3` referenced (python4), use `not six.PY2`"
+YTT203 = "YTT203 `sys.version_info[1]` compared to integer (python4), compare `sys.version_info` to tuple"  # noqa: E501
+YTT204 = "YTT204 `sys.version_info.minor` compared to integer (python4), compare `sys.version_info` to tuple"  # noqa: E501
+YTT301 = "YTT301 `sys.version[0]` referenced (python10), use `sys.version_info`"  # noqa: E501
+YTT302 = "YTT302 `sys.version` compared to string (python10), use `sys.version_info`"  # noqa: E501
+YTT303 = "YTT303 `sys.version[:1]` referenced (python10), use `sys.version_info`"  # noqa: E501
 
 
 def _is_index(node: ast.Subscript, n: int) -> bool:
@@ -42,107 +44,64 @@ class Visitor(ast.NodeVisitor):
     def _is_sys(self, attr: str, node: ast.AST) -> bool:
         return (
             # subscripting `sys.<thing>`
-            isinstance(node, ast.Attribute) and
-            isinstance(node.value, ast.Name) and
-            node.value.id == 'sys' and
-            node.attr == attr
+            isinstance(node, ast.Attribute)
+            and isinstance(node.value, ast.Name)
+            and node.value.id == "sys"
+            and node.attr == attr
         ) or (
-            isinstance(node, ast.Name) and
-            node.id == attr and
-            self._from_imports.get(node.id) == 'sys'
+            isinstance(node, ast.Name)
+            and node.id == attr
+            and self._from_imports.get(node.id) == "sys"
         )
 
     def _is_sys_version_upper_slice(self, node: ast.Subscript, n: int) -> bool:
         return (
-            self._is_sys('version', node.value) and
-            isinstance(node.slice, ast.Slice) and
-            node.slice.lower is None and
-            isinstance(node.slice.upper, ast.Num) and
-            node.slice.upper.n == n and
-            node.slice.step is None
+            self._is_sys("version", node.value)
+            and isinstance(node.slice, ast.Slice)
+            and node.slice.lower is None
+            and isinstance(node.slice.upper, ast.Num)
+            and node.slice.upper.n == n
+            and node.slice.step is None
         )
 
     def visit_Subscript(self, node: ast.Subscript) -> None:
         if self._is_sys_version_upper_slice(node, 1):
-            self.errors.append((
-                node.value.lineno, node.value.col_offset, YTT303,
-            ))
+            self.errors.append(
+                (
+                    node.value.lineno,
+                    node.value.col_offset,
+                    YTT303,
+                ),
+            )
         elif self._is_sys_version_upper_slice(node, 3):
-            self.errors.append((
-                node.value.lineno, node.value.col_offset, YTT101,
-            ))
-        elif self._is_sys('version', node.value) and _is_index(node, n=2):
-            self.errors.append((
-                node.value.lineno, node.value.col_offset, YTT102,
-            ))
-        elif self._is_sys('version', node.value) and _is_index(node, n=0):
-            self.errors.append((
-                node.value.lineno, node.value.col_offset, YTT301,
-            ))
+            self.errors.append(
+                (
+                    node.value.lineno,
+                    node.value.col_offset,
+                    YTT101,
+                ),
+            )
+        elif self._is_sys("version", node.value) and _is_index(node, n=2):
+            self.errors.append(
+                (
+                    node.value.lineno,
+                    node.value.col_offset,
+                    YTT102,
+                ),
+            )
+        elif self._is_sys("version", node.value) and _is_index(node, n=0):
+            self.errors.append(
+                (
+                    node.value.lineno,
+                    node.value.col_offset,
+                    YTT301,
+                ),
+            )
 
-        self.generic_visit(node)
-
-    def visit_Compare(self, node: ast.Compare) -> None:
-        if (
-                isinstance(node.left, ast.Subscript) and
-                self._is_sys('version_info', node.left.value) and
-                _is_index(node.left, n=0) and
-                len(node.ops) == 1 and
-                isinstance(node.ops[0], (ast.Eq, ast.NotEq)) and
-                isinstance(node.comparators[0], ast.Num) and
-                node.comparators[0].n == 3
-        ):
-            self.errors.append((
-                node.left.lineno, node.left.col_offset, YTT201,
-            ))
-        elif (
-                self._is_sys('version', node.left) and
-                len(node.ops) == 1 and
-                isinstance(node.ops[0], (ast.Lt, ast.LtE, ast.Gt, ast.GtE)) and
-                isinstance(node.comparators[0], ast.Str)
-        ):
-            if len(node.comparators[0].s) == 1:
-                code = YTT302
-            else:
-                code = YTT103
-            self.errors.append((
-                node.left.lineno, node.left.col_offset, code,
-            ))
-        elif (
-                isinstance(node.left, ast.Subscript) and
-                self._is_sys('version_info', node.left.value) and
-                _is_index(node.left, n=1) and
-                len(node.ops) == 1 and
-                isinstance(node.ops[0], (ast.Lt, ast.LtE, ast.Gt, ast.GtE)) and
-                isinstance(node.comparators[0], ast.Num)
-        ):
-            self.errors.append((
-                node.lineno, node.col_offset, YTT203,
-            ))
-        elif (
-                isinstance(node.left, ast.Attribute) and
-                self._is_sys('version_info', node.left.value) and
-                node.left.attr == 'minor' and
-                len(node.ops) == 1 and
-                isinstance(node.ops[0], (ast.Lt, ast.LtE, ast.Gt, ast.GtE)) and
-                isinstance(node.comparators[0], ast.Num)
-        ):
-            self.errors.append((
-                node.lineno, node.col_offset, YTT204,
-            ))
-        self.generic_visit(node)
-
-    def visit_Attribute(self, node: ast.Attribute) -> None:
-        if (
-                isinstance(node.value, ast.Name) and
-                node.value.id == 'six' and
-                node.attr == 'PY3'
-        ):
-            self.errors.append((node.lineno, node.col_offset, YTT202))
         self.generic_visit(node)
 
     def visit_Name(self, node: ast.Name) -> None:
-        if node.id == 'PY3' and self._from_imports.get(node.id) == 'six':
+        if node.id == "PY3" and self._from_imports.get(node.id) == "six":
             self.errors.append((node.lineno, node.col_offset, YTT202))
         self.generic_visit(node)
 
