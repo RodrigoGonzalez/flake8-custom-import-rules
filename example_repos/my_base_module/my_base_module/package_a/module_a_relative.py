095078d9fbf90952322b4c7860f8f2ccb11e38b7
Rodrigo Gonzalez <r@rodrigo-gonzalez.com>
2023-07-16 04:44:52 -0400

    refactor(example-package): updating imports and docstrings (#19)

--- example_repos/my_base_module/my_base_module/package_a/module_a_relative.py
+++ example_repos/my_base_module/my_base_module/package_a/module_a_relative.py
@@ -1,3 +1,4 @@
+""" A basic module. """
 from datetime import datetime
 from uuid import UUID
 from uuid import uuid4
@@ -15,3 +16,19 @@ class ARelative:
     _name: str | None = field(default="name")
     _description: str | None = field(default="description")
     _created_at: datetime | None = field(factory=pendulum.now)
+
+    def id(self):
+        """Get the id."""
+        return self._id
+
+    def name(self):
+        """Get the name."""
+        return self._name
+
+    def description(self):
+        """Get the description."""
+        return self._description
+
+    def created_at(self):
+        """Get the created_at."""
+        return self._created_at
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+""" Pytest configuration file. """
 import configparser
 
 import pytest
@@ -18,7 +19,7 @@ def load_config(path: str) -> configparser.RawConfigParser:
 # 		try:
 # 			cfg.read(cfg_path, encoding="UTF-8")
 # 		except (UnicodeDecodeError, configparser.ParsingError) as e:
-# 			LOG.warning("ignoring unparseable config %s: %s", cfg_path, e)
+# 			LOG.warning("ignoring not parseable config %s: %s", cfg_path, e)
 # 		else:
 # 			# only consider it a config if it contains flake8 sections
 # 			if "flake8" in cfg or "flake8:local-plugins" in cfg:
@@ -29,3 +30,22 @@ def load_config(path: str) -> configparser.RawConfigParser:
 def import_visitor():
     """Return an instance of the public import visitor."""
     return ImportVisitor(["application_module", "tests"], ["company_module"])
+
+
+@pytest.fixture(scope="session")
+def list_flake8_extensions():
+    """List all installed flake8 extensions."""
+    from pkg_resources import iter_entry_points
+
+    extensions = []
+    for entry_point in iter_entry_points(group="flake8.extension"):
+        extensions.append(entry_point.name)
+
+    print(extensions)
+    return extensions
+
+
+def pytest_configure(config):
+    """Register markers for pytest."""
+    config.addinivalue_line("markers", "flake8: mark test to run flake8 checks.")
+    config.addinivalue_line("markers", "pylama: mark test to run pylama checks.")
