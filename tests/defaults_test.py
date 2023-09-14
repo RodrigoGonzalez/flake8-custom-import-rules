01ffb590c80cb43559b1460a32c56db5be480594
Rodrigo Gonzalez <r@rodrigo-gonzalez.com>
2023-09-14 15:03:14 -0400

    test(defaults): update deps, add addition code coverate

--- tests/defaults_test.py
+++ tests/defaults_test.py
@@ -3,13 +3,23 @@
 To run this test file only:
 poetry run python -m pytest -vvvrca tests/defaults_test.py
 """
+import optparse
 from collections import defaultdict
 
 import pytest
+from flake8.options.manager import OptionManager
 
+from flake8_custom_import_rules.defaults import CUSTOM_IMPORT_RULES
+from flake8_custom_import_rules.defaults import ERROR_CODES
+from flake8_custom_import_rules.defaults import HELP_STRINGS
+from flake8_custom_import_rules.defaults import POTENTIAL_DYNAMIC_IMPORTS
+from flake8_custom_import_rules.defaults import STANDARD_PROJECT_LEVEL_RESTRICTION_KEYS
+from flake8_custom_import_rules.defaults import STDIN_IDENTIFIERS
+from flake8_custom_import_rules.defaults import Settings
 from flake8_custom_import_rules.defaults import convert_to_dict
 from flake8_custom_import_rules.defaults import convert_to_list
 from flake8_custom_import_rules.defaults import register_opt
+from flake8_custom_import_rules.defaults import register_options
 
 
 @pytest.mark.parametrize(
@@ -105,9 +115,69 @@ def test_convert_to_dict(test_case, delimiter, expected):
     assert actual == expected
 
 
+@pytest.mark.parametrize(
+    ("constant", "expected_type"),
+    [
+        (POTENTIAL_DYNAMIC_IMPORTS, set),
+        (STDIN_IDENTIFIERS, set),
+        (STANDARD_PROJECT_LEVEL_RESTRICTION_KEYS, list),
+        (CUSTOM_IMPORT_RULES, list),
+        (HELP_STRINGS, dict),
+        (ERROR_CODES, dict),
+    ],
+)
+def test_constant_types(constant, expected_type):
+    """Test constant types."""
+    assert isinstance(constant, expected_type)
+
+
+def test_key_not_present():
+    """Test keys or string is present."""
+    with pytest.raises(KeyError):
+        settings = Settings()
+        settings.get_settings_value("some_not_present_key")
+
+
+@pytest.mark.parametrize(
+    ("key", "option_default_value", "is_restriction"),
+    [
+        ("RESTRICT_RELATIVE_IMPORTS", 123, False),
+        ("RESTRICT_RELATIVE_IMPORTS", [], False),
+        ("CUSTOM_RESTRICTIONS", True, False),
+        ("CUSTOM_RESTRICTIONS", 123, False),
+        ("CUSTOM_RESTRICTIONS", [], False),
+    ],
+)
+def test_register_options_raises_type_error(mocker, key, option_default_value, is_restriction):
+    """
+    Test that the `register_options` function raises a TypeError when given
+    incorrect types for `option_default_value`.
+
+    Parameters
+    ----------
+    mocker : pytest_mock.plugin.MockerFixture
+        The mocker fixture for mocking objects.
+    key : str
+        The key representing the setting to be registered.
+    option_default_value : Any
+        The incorrect default value that should raise a TypeError.
+    is_restriction : bool
+        Flag to indicate whether the setting is a restriction or a rule.
+    """
+    mock_option_manager = mocker.Mock(spec=OptionManager)
+
+    with pytest.raises(TypeError):
+        register_options(
+            mock_option_manager,
+            item=key,
+            option_default_value=option_default_value,
+            is_restriction=is_restriction,
+        )
+
+
 def test_register_opt_for_flake8_3x(mocker):
     """Test register_opt for flake8 3.x registration."""
-    mock_option_manager = mocker.Mock()
+    mock_option_manager = mocker.Mock(spec=OptionManager)
     args = ("--example",)
     kwargs = {"action": "store_true"}
 
@@ -117,31 +187,30 @@ def test_register_opt_for_flake8_3x(mocker):
     mock_option_manager.add_option.assert_called_once_with(*args, **kwargs)
 
 
-# Fix later
+def test_register_opt_for_flake8_2x(mocker):
+    """Test register_opt for flake8 2.x registration."""
 
-# def test_register_opt_for_flake8_2x(mocker):
-#     """Test register_opt for flake8 2.x registration."""
-#     mock_option_manager = mocker.Mock()
-#     args = ("--example",)
-#     kwargs = {
-#         "action": "store_true",
-#         "parse_from_config": True,
-#         "comma_separated_list": True,
-#         "normalize_paths": True,
-#     }
-#
-#     # Define a side effect function to simulate exceptions
-#     def side_effect(*args, **kwargs):
-#         # Raise optparse.OptionError first, then TypeError
-#         raise optparse.OptionError("error", "--example")
-#         raise TypeError
-#
-#     # Use an iterator to ensure that the exceptions are raised in sequence
-#     mock_option_manager.add_option.side_effect = iter(side_effect, None)
-#
-#     register_opt(mock_option_manager, *args, **kwargs)
-#
-#     # Check if the add_option method was called without the specific kwargs
-#     reduced_kwargs = {"action": "store_true"}
-#     mock_option_manager.add_option.assert_called_with(*args, **reduced_kwargs)
-#     assert args[-1].lstrip("-") in mock_option_manager.config_options
+    def test_register_opt_handles_option_error(mocker):
+        """
+        Test that the `register_opt` function correctly handles optparse.OptionError.
+        """
+        # Create a mock instance of OptionManager
+        mock_option_manager = mocker.Mock(spec=OptionManager)
+        args = ("--example",)
+        kwargs = {"action": "store_true"}
+
+        # Make add_option raise optparse.OptionError when called
+        mock_option_manager.add_option.side_effect = optparse.OptionError(
+            "some error", "some option"
+        )
+
+        # Call register_opt
+        try:
+            register_opt(
+                mock_option_manager, *args, **kwargs
+            )  # replace args and kwargs with actual values you want to test
+        except Exception as e:
+            pytest.fail(f"register_opt should not raise an exception, but got {e}")
+
+        # Assert that add_option was called twice: once for 3.x and once for 2.x
+        assert mock_option_manager.add_option.call_count == 2
